6b0e9006a3a320a2b04ef9627cba997eb364b2cc
Trathen231 M <trathen231@gmail.com>
2023-08-06 06:47:43 +0000

    Java RemoveAllCharacter

--- src/main/java/RemoveAllCharacter.java
+++ src/main/java/RemoveAllCharacter.java
@@ -1,17 +1,19 @@
-
 public class RemoveAllCharacter {
-    /**
-     * Return a String that has all the character of a String removed.
-     * You could either use a for loop to build a new String that excludes a character, or you could cleverly use
-     * the replaceAll method of String to replace a character with a blank space.
-     *
-     * Here are the docs for String: https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
-     *
-     * @param str A string.
-     * @param ch A string containing a single character which should be removed from str.
-     * @return str, without any characters of type ch.
-     */
-    public String removeAll(String str, String ch){
-        return null;
+
+    public static String removeAll(String str, String ch) {
+        String newStr = "";
+        for (char c : str.toCharArray()) {
+            if (c != ch.charAt(0)) {
+                newStr += c;
+            }
+        }
+        return newStr;
+    }
+
+    public static void main(String[] args) {
+        String str = "hello world";
+        String ch = "l";
+        String newStr = RemoveAllCharacter.removeAll(str, ch);
+        System.out.println(newStr); // heo word
     }
 }
